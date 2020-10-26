e8d8c0dee718cbdef3383ebb1a5c3926e5030974
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-10-26 22:42:27 +0900

    start setting

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {HashRouter as Router, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      calender
+    </div>
+  );
+}
+
+export default App;

c990efbfcf7d3a1f069ce11688aaabf184c58653
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-11-14 14:50:41 +0900

    code refactory (hooks로 변형, 클래스형 주석처리)

--- src/routes/Detail/DetailContainer.js
+++ src/routes/Detail/DetailContainer.js
@@ -1,61 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getImages } from "../../api";
 import DetailPresenter from "./DetailPresenter";
 
- let Key =window.location.href.split("/#/")[1];
 
+const DetailContainer =()=>{
+    let Key =window.location.href.split("/#/")[1];
+    const [ data,setData]=useState(null);
+    const [ loading,setLoader] = useState(true);
+    const [ error,setError] =useState(null);
+    const [ planner,setPlanner] =useState(false);
+    const [ planList,setPlanList] = useState([]);
 
-export default class extends React.Component{
-    state={
-        data:null,
-        loading:true,
-        error:null,
-        planner:false,
-        planList:[]
-    }
-    handleSubmit=  (e)=>{
-        const {planList} = this.state;
+    const handleSubmit = (e) =>{
         e.preventDefault();
         const plan = e.target.firstChild.value;
-        const fixedPlan = planList.typeof === "string" ? [planList] :planList;
-        // state 업데이트
-        this.setState({planList:[plan,...fixedPlan]});
+        const fixedPlan = planList.typeof === "string" ? [planList]:planList;
+        setPlanList(fixedPlan);
         e.target.firstChild.value="";
     }
-    componentDidUpdate(){
+
+    const handleDelClick =(e)=>{
+        const{parentNode:{innerText}} =e.target;
+        const number=parseInt(innerText.split(".")[0]);
+        const newPlans = this.state.planList.filter((item,index)=>index !== number-1);
+        setPlanList(newPlans);       
+        cleanLocalStorage();
+
+    }
+
+    const handleClick=()=>{
+        if (!planner) 
+            setPlanner(true);
+        else
+            setPlanner(false);
+    }
+
+    const cleanLocalStorage = ()=>{
         Key =window.location.href.split("/#/")[1];
-        if(this.state.planList.length !==0)
-            localStorage.setItem(Key,this.state.planList);
+        if(planList.length !==0)
+            localStorage.setItem(Key,planList);
         else
             localStorage.removeItem(Key);
     }
 
-    handleDelClick =(e)=>{
-        const{parentNode:{innerText}} =e.target;
-        console.log(innerText);
-        const number=parseInt(innerText.split(".")[0]);
-        const newPlans = this.state.planList.filter((item,index)=>index !== number-1);
-        console.log(newPlans);
-        this.setState({planList:newPlans});        
-    }
-
-
-    handleClick=()=>{
-        const { planner}=this.state
-        if (!planner) 
-            this.setState({planner:true});
-        else
-            this.setState({planner:false});
-    }
-   
-    async componentDidMount(){
-        const Key =window.location.href.split("/#/")[1];
+    const settingInitial = async ()=>{
         const KeyArray =Key.split("-");
         const Day = new Date();
         const nowYear = Day.getFullYear();
         const nowMonth = Day.getMonth()+1;
-        const nowDay = Day.getDay();
+        const nowDay = Day.getDate();
         let IsSearch = true;
+        // 지금 현재의 날짜보다  미래이면  api로 데이터를 받아올 수 없다. 
         if( nowYear === parseInt(KeyArray[0]))
             IsSearch =nowMonth >=parseInt(KeyArray[1]) && nowDay >=parseInt(KeyArray[2]);
         try{
@@ -64,32 +59,124 @@ export default class extends React.Component{
                 const {data} = await getImages.images(Key);
                 newData=data;
             }else
-                 newData=null;
+                newData=null;
 
-            const plans = localStorage.getItem(Key) ? localStorage.getItem(Key).split(",") : [];
-               
-            this.setState({data:newData,planList:(plans)});
-
+        const plans = localStorage.getItem(Key) ? localStorage.getItem(Key).split(",") : [];
+        setData(newData);
+        setPlanList(plans);
         }catch(error){
-            this.setState({error:"Can't find Image"})
+            setError("Can't find Info");
         }finally{
-            this.setState({loading:false})
+            setLoader(false);
         }
     }
 
-    render(){
-        const {data,loading,error,planner,planList}=this.state
+    useEffect(()=>{
+        console.log("실행");
+        settingInitial();
+    },[]);
+
+
+    return <DetailPresenter
+    data={data}
+    loading={loading}
+    error={error}
+    planner={planner}
+    planList={planList}
+    handleClick={handleClick}
+    handleSubmit={handleSubmit}
+    handleDelClick={handleDelClick}
+    ></DetailPresenter>
+} 
+
+export default DetailContainer;
+
+// export default class extends React.Component{
+//     state={
+//         data:null,
+//         loading:true,
+//         error:null,
+//         planner:false,
+//         planList:[]
+//     }
+//     handleSubmit=  (e)=>{
+//         const {planList} = this.state;
+//         e.preventDefault();
+//         const plan = e.target.firstChild.value;
+//         const fixedPlan = planList.typeof === "string" ? [planList] :planList;
+//         // state 업데이트
+//         this.setState({planList:[plan,...fixedPlan]});
+//         e.target.firstChild.value="";
+//     }
+//     componentDidUpdate(){
+//         Key =window.location.href.split("/#/")[1];
+//         if(this.state.planList.length !==0)
+//             localStorage.setItem(Key,this.state.planList);
+//         else
+//             localStorage.removeItem(Key);
+//     }
+
+//     handleDelClick =(e)=>{
+//         const{parentNode:{innerText}} =e.target;
+//         console.log(innerText);
+//         const number=parseInt(innerText.split(".")[0]);
+//         const newPlans = this.state.planList.filter((item,index)=>index !== number-1);
+//         console.log(newPlans);
+//         this.setState({planList:newPlans});        
+//     }
+
+
+//     handleClick=()=>{
+//         const { planner}=this.state
+//         if (!planner) 
+//             this.setState({planner:true});
+//         else
+//             this.setState({planner:false});
+//     }
+   
+//     async componentDidMount(){
+//         const Key =window.location.href.split("/#/")[1];
+//         const KeyArray =Key.split("-");
+//         const Day = new Date();
+//         const nowYear = Day.getFullYear();
+//         const nowMonth = Day.getMonth()+1;
+//         const nowDay = Day.getDate();
+//         let IsSearch = true;
+ 
+//         if( nowYear === parseInt(KeyArray[0]))
+//             IsSearch =nowMonth >=parseInt(KeyArray[1]) && nowDay >=parseInt(KeyArray[2]);
+//         try{
+//             let newData;    
+//             if(IsSearch){
+//                 const {data} = await getImages.images(Key);
+//                 newData=data;
+//             }else
+//                  newData=null;
+
+//             const plans = localStorage.getItem(Key) ? localStorage.getItem(Key).split(",") : [];
+               
+//             this.setState({data:newData,planList:(plans)});
+
+//         }catch(error){
+//             this.setState({error:"Can't find Image"})
+//         }finally{
+//             this.setState({loading:false})
+//         }
+//     }
+
+//     render(){
+//         const {data,loading,error,planner,planList}=this.state
       
-        return <DetailPresenter
-            data={data}
-            loading={loading}
-            error={error}
-            planner={planner}
-            planList={planList}
-            handleClick={this.handleClick}
-            handleSubmit={this.handleSubmit}
-            handleDelClick={this.handleDelClick}
+//         return <DetailPresenter
+//             data={data}
+//             loading={loading}
+//             error={error}
+//             planner={planner}
+//             planList={planList}
+//             handleClick={this.handleClick}
+//             handleSubmit={this.handleSubmit}
+//             handleDelClick={this.handleDelClick}
            
-        ></DetailPresenter>
-    }
-}
+//         ></DetailPresenter>
+//     }
+// }
--- src/routes/Home/HomeContainer.js
+++ src/routes/Home/HomeContainer.js
@@ -1,160 +1,270 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { getImages } from "../../api";
 import HomePresenter from "./Homepresenter";
-const day =new Date();
-export default class extends React.Component{
-    
-    state={
-        //year,month day는 서브밋할때 마다 업데이트
-        year: day.getFullYear(),
-        month:day.getMonth()+1,
-        day:null,
-        yearArray:[],
-        monthArray:[1,2,3,4,5,6,7,8,9,10,11,12],
-        dayArray:[],
-        startIndex:null,
-        importBtn:0
-    }
 
-    handleImportant=(e)=>{
-        const {importBtn}=this.state;
+
+const HomeContainer =()=>{
+    const Initial =new Date();
+    const [year,setYear]=useState(Initial.getFullYear());
+    const [ month,setMonth]=useState(Initial.getMonth()+1);
+    const [day,setDay] =useState(null);
+    const [yearArray,setYarray]=useState([]);
+    const [monthArray,setMarray]=useState([1,2,3,4,5,6,7,8,9,10,11,12]);
+    const [dayArray,setDarray] =useState([]);
+    const [ startIndex,setStart]=useState(null);
+    const [importBtn,setBtn]=useState(0); 
+
+    const handleImportant=(e) =>{
         if(importBtn === 0){
-          e.target.innerHTML="중요날짜 선택<br/>ON";
-          this.setState({importBtn:1});  
-          console.log(importBtn);
-        }else{
+            e.target.innerHTML="중요날짜 선택<br/>ON";
+            setStart(1);
+    
+        }
+        else{
             e.target.innerHTML="중요날짜 선택<br/>OFF";
-            this.setState({importBtn:0});
-          console.log(importBtn);
-
+            setStart(0);
         }
     }
 
-
-    handleLocalStorage=(key)=>{
+    const handleLocalStorage =(key)=>{
         const list = (localStorage.getItem(key));
         return list ? list.split(",") : [];
     }
 
 
-    
-    calculateDay=(year,month)=>{
+    const calculateDay=(year,month)=>{
         let newArray=[];
 
-        const lastDay=new Date(year,month,0).getDate();
-        const startIndex=new Date(year,month-1,1).getDay();
+        const thisMonthLastDay=new Date(year,month,0).getDate(); //이번달의  마지막 일(30,31 이런것)
+        const thisMonthStartIndex=new Date(year,month-1,1).getDay(); // 처음시작하는 요일 일요일이면 0 
 
-        const lastMonth = 
+        const lasMonthLastDay =  // 저번달의 마지막 일  1월이면  작년 12월계산  
                 month === 1 ? new Date(year-1,12,0).getDate() :new Date(year,month-1,0).getDate();
-        let fillLastDay= lastMonth-(startIndex-1);
+        
+        let fillLastDay= lasMonthLastDay-(thisMonthStartIndex-1); // 전달의 start (27 28 29 30 1 2 3 이러면 27)
     
-        const ThisMonth = new Date(year,month,0).getDay();
+        const nextMonthStartIndex = new Date(year,month,0).getDay(); // 다음달의 시작 요일 
 
-        for(let i=0; i<startIndex; i++){
+        // 전달 의 숫자들을 채워준다. 27 28 29 30 같은 숫자들
+        for(let i=0; i<thisMonthStartIndex; i++){
             newArray.push(fillLastDay);
             fillLastDay++;
         }
-        for(let i=1; i<=lastDay; i++){
+
+        // 이번달의 숫자들을 채운다. 
+        for(let i=1; i<=thisMonthLastDay; i++){
             newArray.push(i);
         }
 
-        for(let i=1; i<(6-ThisMonth)+1; i++){
+        //다음달의 숫자들을 채운다.  1,2,3,4,
+        for(let i=1; i<(6-nextMonthStartIndex)+1; i++){
             newArray.push(i);
         }
 
-
-        this.setState({day:lastDay, dayArray:newArray,startIndex,});
+        setDay(thisMonthLastDay); 
+        setDarray(newArray);
+        setStart(thisMonthStartIndex);
     }
 
-
-
-
-    handleChange=(e)=>{
-        const{ year,month,yearArray,dayArray,startIndex}=this.state;
+    const handleChange=(e)=>{
+        //const{ year,month,yearArray,dayArray,startIndex}=this.state;
+        
         if(e.target.name === "year"){
             const newYear=parseInt(e.target.value);
-            this.calculateDay(newYear,month);
-            this.setState({year:newYear});
+            calculateDay(newYear,month);
+            setYear(newYear);
         }    
         else if(e.target.name==="month"){
             const newMonth = parseInt(e.target.value);
-            this.calculateDay(year,newMonth);
-            this.setState({month:newMonth});
+            calculateDay(year,newMonth);
+            setMonth(newMonth);
         }      
-    
-    }    
-    componentDidUpdate(){
-        // console.log(this.state.year, this.state.month);
-        // console.log(this.state.dayArray.length);
-        // console.log( this.state.startIndex);
-        //console.log("업데이트후",this.state.importBtn)
     }
+    
+    useEffect(()=>{
+        calculateDay(year,month);
+        for(let i=1997; i<=year; i++)
+            yearArray.push(i);   
+        yearArray.reverse();
+        setYarray(yearArray);
+    },[]);
 
-     componentDidMount(){
-        //마지막 날짜 구하기 
-        const{ year,month,day,yearArray,dayArray,startIndex}=this.state;
-        const newStartIndex=new Date(year,month-1,1).getDay();
+    return <HomePresenter
+    year={year}
+    month={month}
+    day={day}
+    yearArray={yearArray}
+    monthArray={monthArray}
+    dayArray={dayArray}
+    startIndex={startIndex}
+    importBtn={importBtn}
+    handleChange={handleChange}
+    handleLocalStorage={handleLocalStorage}
+    handleImportant={handleImportant}
+    ></HomePresenter>
+}
+
+export default HomeContainer;
+
+
+// export default class extends React.Component{
+    
+//     state={
+//         //year,month day는 서브밋할때 마다 업데이트
+//         year: day.getFullYear(),
+//         month:day.getMonth()+1,
+//         day:null,
+//         yearArray:[],
+//         monthArray:[1,2,3,4,5,6,7,8,9,10,11,12],
+//         dayArray:[],
+//         startIndex:null,
+//         importBtn:0
+//     }
+
+//     handleImportant=(e)=>{
+//         const {importBtn}=this.state;
+//         if(importBtn === 0){
+//           e.target.innerHTML="중요날짜 선택<br/>ON";
+//           this.setState({importBtn:1});  
+//           console.log(importBtn);
+//         }else{
+//             e.target.innerHTML="중요날짜 선택<br/>OFF";
+//             this.setState({importBtn:0});
+//           console.log(importBtn);
+
+//         }
+//     }
+
+
+//     handleLocalStorage=(key)=>{
+//         const list = (localStorage.getItem(key));
+//         return list ? list.split(",") : [];
+//     }
+
+
+    
+//     calculateDay=(year,month)=>{
+//         let newArray=[];
+
+//         const lastDay=new Date(year,month,0).getDate(); //이번달의  마지막 일(30,31 이런것)
+//         const startIndex=new Date(year,month-1,1).getDay(); // 처음시작하는 요일 일요일이면 0 
+
+//         const lastMonth =  // 저번달의 마지막 일  1월이면  작년 12월계산  
+//                 month === 1 ? new Date(year-1,12,0).getDate() :new Date(year,month-1,0).getDate();
+        
+//         let fillLastDay= lastMonth-(startIndex-1); // 전달의 start (27 28 29 30 1 2 3 이러면 27)
+    
+//         const ThisMonth = new Date(year,month,0).getDay(); // 다음달의 시작 요일 
+
+//         // 전달 의 숫자들을 채워준다. 27 28 29 30 같은 숫자들
+//         for(let i=0; i<startIndex; i++){
+//             newArray.push(fillLastDay);
+//             fillLastDay++;
+//         }
+
+//         // 이번달의 숫자들을 채운다. 
+//         for(let i=1; i<=lastDay; i++){
+//             newArray.push(i);
+//         }
+
+//         //다음달의 숫자들을 채운다.  1,2,3,4,
+//         for(let i=1; i<(6-ThisMonth)+1; i++){
+//             newArray.push(i);
+//         }
+
+
+//         this.setState({day:lastDay, dayArray:newArray,startIndex,});
+//     }
+
+
+
+
+//     handleChange=(e)=>{
+//         const{ year,month,yearArray,dayArray,startIndex}=this.state;
+//         if(e.target.name === "year"){
+//             const newYear=parseInt(e.target.value);
+//             this.calculateDay(newYear,month);
+//             this.setState({year:newYear});
+//         }    
+//         else if(e.target.name==="month"){
+//             const newMonth = parseInt(e.target.value);
+//             this.calculateDay(year,newMonth);
+//             this.setState({month:newMonth});
+//         }      
+    
+//     }    
+//     componentDidUpdate(){
+//         // console.log(this.state.year, this.state.month);
+//         // console.log(this.state.dayArray.length);
+//         // console.log( this.state.startIndex);
+//         //console.log("업데이트후",this.state.importBtn)
+//     }
+
+//      componentDidMount(){
+//         //마지막 날짜 구하기 
+//         const{ year,month,day,yearArray,dayArray,startIndex}=this.state;
+//         const newStartIndex=new Date(year,month-1,1).getDay();
             
-        const lastDay=new Date(year,month,0).getDate();
+//         const lastDay=new Date(year,month,0).getDate();
         
-        // 이번달의 1일이 목요일부터 시작하면
-        // 일 월 화 수는 저번달의 숫자로 채워야한다
-        // 따라서 fillLastDay는  일 월 화 수 중에  일요일에
-        //해당하는 일수 를 나타내며 하나씩 증가시키면서 배열에 넣어준다.
-        const lastMonth = 
-            month === 1 ? new Date(year-1,11,0).getDate() :new Date(year,month-1,0).getDate();
-        let fillLastDay= lastMonth-(newStartIndex-1);
+//         // 이번달의 1일이 목요일부터 시작하면
+//         // 일 월 화 수는 저번달의 숫자로 채워야한다
+//         // 따라서 fillLastDay는  일 월 화 수 중에  일요일에
+//         //해당하는 일수 를 나타내며 하나씩 증가시키면서 배열에 넣어준다.
+//         const lastMonth = 
+//             month === 1 ? new Date(year-1,11,0).getDate() :new Date(year,month-1,0).getDate();
+//         let fillLastDay= lastMonth-(newStartIndex-1);
         
-        //마찬가지로  이번달의 마지막 주에는  다음달의
-        // 날짜도 포함되야 한다.
+//         //마찬가지로  이번달의 마지막 주에는  다음달의
+//         // 날짜도 포함되야 한다.
 
-        const ThisMonth = new Date(year,month,0).getDay();
+//         const ThisMonth = new Date(year,month,0).getDay();
       
 
-        for(let i=1997; i<=year; i++){
-            yearArray.push(i);   
-        }
-        yearArray.reverse();
+//         for(let i=1997; i<=year; i++){
+//             yearArray.push(i);   
+//         }
+//         yearArray.reverse();
         
-        // 저번달 수를 채운다. (첫째주)
-        for(let i=0; i<newStartIndex; i++){
-            dayArray.push(fillLastDay);
-            fillLastDay++;
-        }
-        // 이번달 수를 채운다.
-        for(let i=1; i<=lastDay; i++){
-            dayArray.push(i);
-        }
+//         // 저번달 수를 채운다. (첫째주)
+//         for(let i=0; i<newStartIndex; i++){
+//             dayArray.push(fillLastDay);
+//             fillLastDay++;
+//         }
+//         // 이번달 수를 채운다.
+//         for(let i=1; i<=lastDay; i++){
+//             dayArray.push(i);
+//         }
 
-        // 다음달 수를 채운다. 마지막주
+//         // 다음달 수를 채운다. 마지막주
 
-        for(let i=1; i<(6-ThisMonth)+1; i++){
-            dayArray.push(i);
-        }
+//         for(let i=1; i<(6-ThisMonth)+1; i++){
+//             dayArray.push(i);
+//         }
        
 
         
-        this.setState({yearArray,dayArray,day:lastDay,startIndex:newStartIndex});
-    }
+//         this.setState({yearArray,dayArray,day:lastDay,startIndex:newStartIndex});
+//     }
 
 
-    render(){
-        const {year,month,day,yearArray,monthArray,dayArray,startIndex,importBtn}=this.state;
-        //console.log("render",this.state.importBtn);
-        return <HomePresenter
-        year={year}
-        month={month}
-        day={day}
-        yearArray={yearArray}
-        monthArray={monthArray}
-        dayArray={dayArray}
-        startIndex={startIndex}
-        importBtn={importBtn}
-        handleChange={this.handleChange}
-        handleLocalStorage={this.handleLocalStorage}
-        handleImportant={this.handleImportant}
-        ></HomePresenter>
-    }
+//     render(){
+//         const {year,month,day,yearArray,monthArray,dayArray,startIndex,importBtn}=this.state;
+//         //console.log("render",this.state.importBtn);
+//         return <HomePresenter
+//         year={year}
+//         month={month}
+//         day={day}
+//         yearArray={yearArray}
+//         monthArray={monthArray}
+//         dayArray={dayArray}
+//         startIndex={startIndex}
+//         importBtn={importBtn}
+//         handleChange={this.handleChange}
+//         handleLocalStorage={this.handleLocalStorage}
+//         handleImportant={this.handleImportant}
+//         ></HomePresenter>
+//     }
 
-}
+// }
 
